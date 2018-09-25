8a95c90aa0ff8adb0675af882f8a501aa20a9fef
Motherbox <danielohly@gmail.com>
2018-09-25 11:51:58 -0500

    heroku11

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/welcometo'));
+app.use(express.static('./dist/welcometop2g5'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'./dist/welcometo/src/index.html'));
+res.sendFile(path.join(__dirname,'/dist/welcometop2g5/src/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
