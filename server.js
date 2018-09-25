2033208f1cdaf20028c0694966bfa7d776543d57
Motherbox <danielohly@gmail.com>
2018-09-25 11:32:38 -0500

    heroku functionality9

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static('./dist/welcometo'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/welcometo/src/index.html'));
+res.sendFile(path.join(__dirname,'./dist/welcometo/src/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
